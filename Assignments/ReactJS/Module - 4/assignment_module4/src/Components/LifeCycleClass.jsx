49866f52c7d54cba0c91b13f7e3ffdf8b07e6f73
priyeshkhamar <priyeshkhamar76@gmail.com>
2024-06-01 12:43:23 +0530

    Update LifeCycleClass.jsx

--- Assignments/ReactJS/Module - 4/assignment_module4/src/Components/LifeCycleClass.jsx
+++ Assignments/ReactJS/Module - 4/assignment_module4/src/Components/LifeCycleClass.jsx
@@ -1,53 +1,56 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class LifeCycleClass extends Component {
     constructor() {
-        super()
+        super();
         this.state = {
             rollno: 1,
-            name: "Testing"
-        }
-        console.log("Constructor Called..." + this.state.name);
+            name: 'Testing',
+        };
+        console.log('Constructor Called... ' + this.state.name);
     }
 
     componentDidMount() {
         setTimeout(() => {
-            this.setState({ Name: "Test 2" });
-            console.log("Component Did Mount Called...");
+            this.setState({ name: 'Test 2' });
+            console.log('Component Did Mount Called...');
         }, 5000);
     }
 
     static getDerivedStateFromProps(props, state) {
-        console.log("Get derived state from props called...");
-        state.name = "UpdatedName";
-        return state;
+        console.log('Get derived state from props called...');
+        if (state.name !== 'UpdatedName') {
+            return { name: 'UpdatedName' };
+        }
+        return null; 
     }
 
-    shouldComponentUpdate() {
-        console.log("Should Component Updated!" + this.state.name);
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('Should Component Update! Current name: ' + this.state.name);
         return true;
     }
 
     getSnapshotBeforeUpdate(prevProps, prevState) {
-        console.log("Prev State Name is " + prevState.name);
-        return prevState;
+        console.log('Prev State Name is ' + prevState.name);
+        return null;
     }
 
-    componentDidUpdate() {
-        console.log("Name is " + this.state.name);
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('Name is ' + this.state.name);
     }
+
     componentWillUnmount() {
-        console.log("Destroyed...");
+        console.log('Destroyed...');
     }
 
     render() {
         return (
             <div>
-                <h3>Component LifeCycle : Class Component</h3>
-                <h3>RollNo is : {this.state.rollno}</h3>
-                <h3>Name is : {this.state.name}</h3>
-                <button onClick={() => this.setState({ name: "ABC" })}>Change Name</button>
+                <h3>Component LifeCycle: Class Component</h3>
+                <h3>RollNo is: {this.state.rollno}</h3>
+                <h3>Name is: {this.state.name}</h3>
+                <button onClick={() => this.setState({ name: 'ABC' })}>Change Name</button>
             </div>
-        )
+        );
     }
 }
